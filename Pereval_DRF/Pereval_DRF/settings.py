81896b2f7d7603f53d660d6fcba50038e8e9c308
Александр <7176uiop3436>
2022-09-06 21:50:06 +0300

    Подключаем приложение app_main к списку приложений

--- Pereval_DRF/Pereval_DRF/settings.py
+++ Pereval_DRF/Pereval_DRF/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # подключаем созданное приложение app_main
+    'app_main',
 ]
 
 MIDDLEWARE = [
