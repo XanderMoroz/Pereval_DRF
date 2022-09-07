5e6bd4ae334adcab86bee0d7ed5d00b207b04b5b
Александр <7176uiop3436>
2022-09-07 18:12:54 +0300

    Подключили Postgres 12

--- Pereval_DRF/Pereval_DRF/settings.py
+++ Pereval_DRF/Pereval_DRF/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'Pereval_DRF.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
