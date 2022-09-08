4d95277f592802ed96e9bc7522b2d1a11d33298e
Александр <7176uiop3436>
2022-09-08 18:31:47 +0300

    Первое представление PerevalListView

--- Pereval_DRF/app_main/views.py
+++ Pereval_DRF/app_main/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import *
+from rest_framework.views import APIView
+
+from .serializers import PerevalAddSerializer
+
+
+class PerevalListView(APIView):
+   """Список всех перевалов"""
+
+   def get(self, request):
+      perevals = PerevalAdd.objects.all()
+      serializer = PerevalAddSerializer(perevals, many=True)
+      return Response(serializer.data)
+
+   def post(self, request):
+      pereval = PerevalAddSerializer(data=request.data)
+      if pereval.is_valid():
+         pereval.save()
+         return Response(status=201)
+      else:
+         print("Ошибка валидизации данных")
+         return Response(status=400)
