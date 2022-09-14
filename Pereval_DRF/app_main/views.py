4390bc08399bac4eee7e4ece7d5959064ea955bd
Александр <7176uiop3436>
2022-09-14 20:57:08 +0300

    Создан новый вью: PerevalDetail(на основе PerevalAddSerializer)

--- Pereval_DRF/app_main/views.py
+++ Pereval_DRF/app_main/views.py
@@ -3,15 +3,20 @@ from rest_framework.response import Response
 from django.http import JsonResponse
 from rest_framework import generics
 from .models import *
-from .serializers import PerevalAddSerializer, CoordsSerializer
+from .serializers import PerevalAddSerializer, PerevalDetailSerializer
 
 
 class PerevalAPIView(generics.CreateAPIView):
+    """Класс работы с БД для первого спринта"""
     queryset = PerevalAdd.objects.all()
     serializer_class = PerevalAddSerializer
 
     def post(self, request):
-        """POST запрос для добавления модели"""
+        """
+        Переопределение метода POST
+        :param request: Json для полей модели перевала (PerevalAdd)
+        :return: JsonResponse пример: { "status": 200, "message": null, "id": 42 }
+        """
         pereval = PerevalAddSerializer(data=request.data)
         try:
             if pereval.is_valid(raise_exception=True):
@@ -29,3 +34,29 @@ class PerevalAPIView(generics.CreateAPIView):
                 'id': 'null'
             }
         return JsonResponse(data, status=400, safe=False)
+
+class PerevalDetail(generics.UpdateAPIView):
+    """Класс работы с БД для второго спринта"""
+    serializer_class = PerevalAddSerializer
+
+    def get_object(self, pk):
+        return PerevalAdd.objects.get(pk=pk)
+
+    def get(self, *args, **kwargs):
+        """
+        Переопределение метода GET для вывода записи по id
+        :param request: Json для полей модели перевала (PerevalAdd)
+        :return: JsonResponse пример: {"state": "0", "message": "PerevalAdd matching query does not exist."}
+        """
+        pk = kwargs.get('pk', None)
+        try:
+            data = PerevalDetailSerializer(PerevalAdd.objects.get(pk=pk)).data
+            responseData = {'state': '1', 'message': f'{data}'}
+            return JsonResponse(responseData)
+        except Exception as exc:
+            responseData = {'state': '0', 'message': f'{exc}'}
+            return JsonResponse(responseData)
+
+
+
+
