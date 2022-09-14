a18fe6c460dc56aed6af14ecae69562f774e6132
Александр <7176uiop3436>
2022-09-14 18:30:53 +0300

    Добавил новые серализаторы(User, Coords)

--- Pereval_DRF/app_main/serializers.py
+++ Pereval_DRF/app_main/serializers.py
@@ -1,11 +1,36 @@
 from rest_framework import serializers
 
-from .models import PerevalAdd
+from .models import PerevalAdd, Coords, PerevalUser
+
+class UserSerializer(serializers.ModelSerializer):
+    """Сериализатор пользователей"""
+    class Meta:
+       model = PerevalUser
+       fields = ['fam', 'name', 'otc', 'email', 'phone']
+
+class CoordsSerializer(serializers.ModelSerializer):
+    """Сериализатор координат перевала"""
+    class Meta:
+       model = PerevalUser
+       fields = ['latitude', 'longitude', 'height']
 
 
 class PerevalAddSerializer(serializers.ModelSerializer):
     """Сериализатор перевалов"""
+    user = serializers.PrimaryKeyRelatedField(queryset=PerevalUser.objects.all())
+    coords = serializers.PrimaryKeyRelatedField(queryset=Coords.objects.all())
     class Meta:
         model = PerevalAdd
-        exclude = ("status",)  # все поля кроме статуса
-
+        depth = 1
+        fields = ("beauty_title",
+                  "title",
+                  "other_titles",
+                  "connect",
+                  "add_time",
+                  "user",
+                  "coords",
+                  "level_winter",
+                  "level_summer",
+                  "level_autumn",
+                  "level_spring",
+                  )
