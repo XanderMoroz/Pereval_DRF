000d6d66bacc17b0fd192daa8b903dd9c4b4f5d7
Александр <7176uiop3436>
2022-09-15 21:33:30 +0300

    передал методы 'get' и 'patch' в PerevalDetail

--- Pereval_DRF/app_main/urls.py
+++ Pereval_DRF/app_main/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # Спринт №1
     path('submitData', PerevalAPIView.as_view()),
     # Спринт №2
-    path('submitData/<int:pk>/', PerevalDetail.as_view()),
+    path('submitData/<int:pk>/', PerevalDetail.as_view({'get': 'retrieve', 'patch': 'partial_update'})),
 ]
